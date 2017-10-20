701354f1be0f010a5e190cfa33f3adb83151a133
NealRosenberger <nealarosenberger@gmail.com>
2017-10-20 19:58:51 -0400

    update

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,22 +1,19 @@
 $( document ).ready(function(){
   var Random=Math.floor(Math.random()*101+19)
-  // Selects a random number to be shown at the start of the game
-  // Number should be should be between 19 - 120
-  //
+
+
   $('#randomNumber').text(Random);
-  // Appending random number to the randomNumber id in the html doc
-  //
+
   var num1= Math.floor(Math.random()*11+1)
   var num2= Math.floor(Math.random()*11+1)
   var num3= Math.floor(Math.random()*11+1)
   var num4= Math.floor(Math.random()*11+1)
-  // Setting up random numbers for each jewel
-  // Random number has to be between 1 - 12
-  // 
+
+  
   var userTotal= 0; 
   var wins= 0;
   var losses = 0;
-  //  Decaring variables for tallies
+  
 $('#numberWins').text(wins);
 $('#numberLosses').text(losses);
 //resets the game
@@ -31,26 +28,26 @@ function reset(){
       userTotal= 0;
       $('#finalTotal').text(userTotal);
       } 
-//adds the wins to the userTotal
+//adds  wins 
 function yay(){
 alert("You won!");
   wins++; 
   $('#numberWins').text(wins);
   reset();
 }
-//addes the losses to the userTotal
+//addeslosses 
 function loser(){
 alert ("You lose!");
   losses++;
   $('#numberLosses').text(losses);
   reset()
 }
-//sets up click for jewels
+//sets up click 
   $('#one').on ('click', function(){
     userTotal = userTotal + num1;
     console.log("New userTotal= " + userTotal);
     $('#finalTotal').text(userTotal); 
-          //sets win/lose conditions
+   
         if (userTotal == Random){
           yay();
         }
@@ -73,7 +70,7 @@ alert ("You lose!");
     userTotal = userTotal + num3;
     console.log("New userTotal= " + userTotal);
     $('#finalTotal').text(userTotal);
-//sets win/lose conditions
+
           if (userTotal == Random){
           yay();
         }
